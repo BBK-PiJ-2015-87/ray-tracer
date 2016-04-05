b5d9b5c7b3179aaa7cebb5a53d6411a20f666409
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2016-04-05 11:28:27 +0100

    Created new class RendererEngine and moved rendering logic to it from scene class. Scene class now holds just lights and objects.

--- src/main/scala/Tracer.scala
+++ src/main/scala/Tracer.scala
@@ -1,30 +1,34 @@
-import java.io.File
-
-import com.mildlyskilled.{Scene, Image, Coordinator, Settings}
+import com.mildlyskilled._
 
 object Tracer extends App {
 
   val (inFile, outFile) = ("src/main/resources/input.dat", "output.png")
+  val settings = new Settings
+  val counter = new Counter
+  val camera = new Camera
   val scene = Scene.fromFile(inFile)
-  val counter = scene.counter
-  val settings = scene.settings
 
-  render(scene, outFile, settings.width, settings.height)
+  val renderEngine = new RenderEngine(scene, settings, counter, camera)
+
+  val image = new Image(settings.width, settings.height)
+
+
+  render(scene, outFile, settings)
 
   println("rays cast " + counter.rayCount)
   println("rays hit " + counter.hitCount)
   println("light " + counter.lightCount)
   println("dark " + counter.darkCount)
 
-  def render(scene: Scene, outfile: String, width: Int, height: Int) = {
-    val image = new Image(width, height)
+  def render(scene: Scene, outfile: String, renderSettings: Settings) = {
+
 
     // Init the coordinator -- must be done before starting it.
     Coordinator.init(image, outfile)
 
     // TODO: Start the Coordinator actor.
 
-    scene.traceImage(width, height)
+    renderEngine.render()
 
     // TODO:
     // This one is tricky--we can't simply send a message here to print
